77670e4660c22123f021ee5efd1fbc4fe98fcf4f
xtrisk <163651666+xtrisk@users.noreply.github.com>
2026-04-03 22:50:04 +0600

    Enhance user schema with trustedUsers field
    Added 'trustedUsers' field with default value to user schema.

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,11 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema(
-  {
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    blockedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  },
-  { timestamps: true }
-);
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email:    { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  blockedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', default: [] }],
+  trustedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', default: [] }],
+}, { timestamps: true });
 
 export default mongoose.model('User', userSchema);
